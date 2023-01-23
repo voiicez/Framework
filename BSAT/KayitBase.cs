94b5f0e4f1bb32841b03468568fbc3a0a711c701
behlul <voiicez@gmail.com>
2023-01-23 22:22:07 +0300

    First

---
+++ BSAT/KayitBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BSAT
+{
+	public abstract class KayitBase
+	{
+		[Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+		public Guid Guid { get; set; } = Guid.NewGuid();
+		public bool isDeleted { get; set; } = false;
+	}
+}
+
